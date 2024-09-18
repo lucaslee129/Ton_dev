2251475b7014d67a2f05a427714e6a42ba613efd
Lucas <adfr486233@gmail.com>
2024-09-19 03:11:58 +0800

    feat: redefine the contract

--- my-counter/wrappers/Counter.compile.ts
+++ my-counter/wrappers/Counter.compile.ts
@@ -2,5 +2,5 @@ import { CompilerConfig } from '@ton/blueprint';
 
 export const compile: CompilerConfig = {
     lang: 'func',
-    targets: ['contracts/func_contract.fc'],
+    targets: ['contracts/counter.fc'],
 };
