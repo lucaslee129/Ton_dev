b6673be9cf79a632c07778aee4db51f6429b9624
Lucas <adfr486233@gmail.com>
2024-09-25 03:14:39 +0800

    fix: fix the problem on wrapper script: configToCell function has problem

--- my-counter/tests/AddressSaver.spec.ts
+++ my-counter/tests/AddressSaver.spec.ts
@@ -1,5 +1,5 @@
 import { Blockchain, SandboxContract, TreasuryContract } from '@ton/sandbox';
-import { beginCell, Cell, toNano } from '@ton/core';
+import { Cell, beginCell, toNano } from '@ton/core';
 import { AddressSaver } from '../wrappers/AddressSaver';
 import '@ton/test-utils';
 import { compile } from '@ton/blueprint';
@@ -13,11 +13,12 @@ describe('AddressSaver', () => {
     });
 
     let blockchain: Blockchain;
-    let deployer: SandboxContract<TreasuryContract>;
     let addressSaver: SandboxContract<AddressSaver>;
+    let deployer: SandboxContract<TreasuryContract>;
 
     beforeEach(async () => {
         blockchain = await Blockchain.create();
+
         deployer = await blockchain.treasury('deployer');
 
         addressSaver = blockchain.openContract(
@@ -29,154 +30,68 @@ describe('AddressSaver', () => {
             )
         );
 
-        const deployResult = await addressSaver.sendDeploy(deployer.getSender(), toNano('0.05'));
+        const deployResult = await addressSaver.sendDeploy(deployer.getSender(), toNano('0.01'));
 
         expect(deployResult.transactions).toHaveTransaction({
             from: deployer.address,
             to: addressSaver.address,
             deploy: true,
-            // success: true,
         });
     });
 
+    it('should deploy', async () => {});
+
     it('should change saved address by manager', async () => {
         const address = randomAddress();
-        const result = await addressSaver.sendChangeAddress(
-            deployer.getSender(),
-            toNano('0.01'),
-            12345n,
-            address
-        );
-    
-        console.log("Trasaction as a result", result.transactions);
+        const result = await addressSaver.sendChangeAddress(deployer.getSender(), toNano('0.01'), 12345n, address);
 
-        // expect(result.transactions).toHaveTransaction({
-        //     from: deployer.address,
-        //     to: addressSaver.address,
-        //     success: true,
-        // });
+        expect(result.transactions).toHaveTransaction({
+            from: deployer.address,
+            to: addressSaver.address,
+            success: true,
+        });
     });
 
-    // it('should not change saved address by anyone else', async () => {
-    //     let user = await blockchain.treasury('user');
-    //     const address = randomAddress();
-    //     const result = await addressSaver.sendChangeAddress(
-    //         user.getSender(),
-    //         toNano('0.01'),
-    //         12345n,
-    //         address
-    //     );
-    
-    //     expect(result.transactions).toHaveTransaction({
-    //         from: user.address,
-    //         to: addressSaver.address,
-    //         success: false,
-    //     });
-    // });
+    it('should not change saved address by anyone else', async () => {
+        let user = await blockchain.treasury('user');
+        const address = randomAddress();
+        const result = await addressSaver.sendChangeAddress(user.getSender(), toNano('0.01'), 12345n, address);
 
-    // it('should change saved address by manager', async () => {
-    //     const address = randomAddress();
-    //     const result = await addressSaver.sendChangeAddress(
-    //         deployer.getSender(),
-    //         toNano('0.01'),
-    //         12345n,
-    //         address
-    //     );
+        expect(result.transactions).toHaveTransaction({
+            from: user.address,
+            to: addressSaver.address,
+            success: false,
+        });
+    });
 
-    //     expect(result.transactions).toHaveTransaction({
-    //         from: deployer.address,
-    //         to: addressSaver.address,
-    //         success: true,
-    //     })
-    // });
+    it('should return required data on `requestAddress` call', async () => {
+        const address = randomAddress();
+        await addressSaver.sendChangeAddress(deployer.getSender(), toNano('0.01'), 12345n, address);
 
-    // it('should not change saved address by anyone else', async() => {
-    //     let user = await blockchain.treasury('user');
-    //     const address = randomAddress();
-    //     const result = await addressSaver.sendChangeAddress(
-    //         user.getSender(),
-    //         toNano('0.01'),
-    //         12345n,
-    //         address
-    //     );
+        let user = await blockchain.treasury('user');
+        const result = await addressSaver.sendRequestAddress(user.getSender(), toNano('0.01'), 12345n);
+        expect(result.transactions).toHaveTransaction({
+            from: addressSaver.address,
+            to: user.address,
+            body: beginCell()
+                .storeUint(3, 32)
+                .storeUint(12345n, 64)
+                .storeAddress(deployer.address)
+                .storeAddress(address)
+                .endCell(),
+        });
+    });
 
-    //     expect(result.transactions).toHaveTransaction({
-    //         from: user.address,
-    //         to: addressSaver.address,
-    //         success: false,
-    //     })
-    // });
-
-    // it('should return required data on `requestAddress` call', async () => {
-    //     const address = randomAddress();
-    //     await addressSaver.sendChangeAddress(
-    //         deployer.getSender(),
-    //         toNano('0.01'),
-    //         12345n,
-    //         address
-    //     );
-
-    //     let user = await blockchain.treasury('user');
-    //     const result = await addressSaver.sendRequestAddress(
-    //         user.getSender(),
-    //         toNano('0.01'),
-    //         12345n
-    //     );
-
-    //     expect(result.transactions).toHaveTransaction({
-    //         from : addressSaver.address,
-    //         to: user.address,
-    //         body: beginCell()
-    //             .storeUint(3, 32)
-    //             .storeUint(12345n, 64)
-    //             .storeAddress(deployer.address)
-    //             .storeAddress(address)
-    //             .endCell(),
-    //     });
-    // })
-
-    // it('should return required data on `requestAddress` call', async () => {
-    //     const address = randomAddress();
-    //     await addressSaver.sendChangeAddress(
-    //         deployer.getSender(),
-    //         toNano('0.01'),
-    //         12345n,
-    //         address
-    //     );
-    
-    //     let user = await blockchain.treasury('user');
-    //     const result = await addressSaver.sendRequestAddress(
-    //         user.getSender(),
-    //         toNano('0.01'),
-    //         12345n
-    //     );
-    //     expect(result.transactions).toHaveTransaction({
-    //         from: addressSaver.address,
-    //         to: user.address,
-    //         body: beginCell()
-    //             .storeUint(3, 32)
-    //             .storeUint(12345n, 64)
-    //             .storeAddress(deployer.address)
-    //             .storeAddress(address)
-    //             .endCell(),
-    //     });
-    // });
-
-    // it("should throw on any other opcode", async () => {
-    //     const result = await deployer.send({
-    //         to: addressSaver.address,
-    //         value: toNano('0.01'),
-    //         body: beginCell().storeUint(5, 32).storeUint(12345n, 64).endCell(),
-    //     })
-
-    //     expect(result.transactions).toHaveTransaction({
-    //         from: deployer.address,
-    //         to: addressSaver.address,
-    //         exitCode: 3,
-    //     })
-    // })
-
-    // it('shout test', async () => {
-    //     // console.log(code)
-    // })
+    it('should throw on any other opcode', async () => {
+        const result = await deployer.send({
+            to: addressSaver.address,
+            value: toNano('0.01'),
+            body: beginCell().storeUint(5, 32).storeUint(12345n, 64).endCell(),
+        });
+        expect(result.transactions).toHaveTransaction({
+            from: deployer.address,
+            to: addressSaver.address,
+            exitCode: 3,
+        });
+    });
 });
