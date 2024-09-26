13f5515c7119be70af00433338ade4c271febf7a
Lucas <adfr486233@gmail.com>
2024-09-27 02:30:43 +0800

    fix: fixing the bugs while testing using test script

--- my-counter/jest.config.ts
+++ my-counter/jest.config.ts
@@ -4,6 +4,13 @@ const config: Config = {
     preset: 'ts-jest',
     testEnvironment: 'node',
     testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+    // globals: {
+    //     BigInt: {
+    //       toJSON: function() {
+    //         return this.toString();
+    //       }
+    //     }
+    //   }
 };
 
 export default config;
