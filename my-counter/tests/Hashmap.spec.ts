a1c672b5f272403c50cca0be460243d52b2f5ef5
Lucas <adfr486233@gmail.com>
2024-09-28 00:21:59 +0800

    feat: complete the lesson 8 : test script for hashmap

--- my-counter/tests/Hashmap.spec.ts
+++ my-counter/tests/Hashmap.spec.ts
@@ -18,6 +18,8 @@ describe('Hashmap', () => {
     beforeEach(async () => {
         blockchain = await Blockchain.create();
 
+        blockchain.now = 500;
+
         deployer = await blockchain.treasury('deployer');
 
         hashmap = blockchain.openContract(
@@ -28,8 +30,6 @@ describe('Hashmap', () => {
                 code
             )
         );
-
-        deployer = await blockchain.treasury('deployer');
 
         const deployResult = await hashmap.sendDeploy(deployer.getSender(), toNano('0.01'));
 
@@ -86,60 +86,60 @@ describe('Hashmap', () => {
         await expect(hashmap.getByKey(123n)).rejects.toThrow();
     })
 
-    // it('should clear old values', async () => {
-    //     await hashmap.sendClearOldValues(deployer.getSender(), toNano('0.05'), {
-    //         queryId: 123n,
-    //     });
+    it('should clear old values', async () => {
+        await hashmap.sendClearOldValues(deployer.getSender(), toNano('0.05'), {
+            queryId: 123n,
+        });
     
-    //     let [validUntil, value] = await hashmap.getByKey(1n);
-    //     expect(validUntil).toEqual(1000n);
-    //     expect(value).toEqualSlice(
-    //         beginCell().storeUint(123, 16).endCell().asSlice()
-    //     );
+        let [validUntil, value] = await hashmap.getByKey(1n);
+        expect(validUntil).toEqual(1000n);
+        expect(value).toEqualSlice(
+            beginCell().storeUint(123, 16).endCell().asSlice()
+        );
     
-    //     blockchain.now = 1001;
+        blockchain.now = 1001;
     
-    //     await hashmap.sendClearOldValues(deployer.getSender(), toNano('0.05'), {
-    //         queryId: 123n,
-    //     });
+        await hashmap.sendClearOldValues(deployer.getSender(), toNano('0.05'), {
+            queryId: 123n,
+        });
     
-    //     await expect(hashmap.getByKey(1n)).rejects.toThrow();
+        await expect(hashmap.getByKey(1n)).rejects.toThrow();
     
-    //     [validUntil, value] = await hashmap.getByKey(2n);
-    //     expect(validUntil).toEqual(2000n);
-    //     expect(value).toEqualSlice(
-    //         beginCell().storeUint(234, 16).endCell().asSlice()
-    //     );
+        [validUntil, value] = await hashmap.getByKey(2n);
+        expect(validUntil).toEqual(2000n);
+        expect(value).toEqualSlice(
+            beginCell().storeUint(234, 16).endCell().asSlice()
+        );
     
-    //     [validUntil, value] = await hashmap.getByKey(3n);
-    //     expect(validUntil).toEqual(3000n);
-    //     expect(value).toEqualSlice(
-    //         beginCell().storeUint(345, 16).endCell().asSlice()
-    //     );
+        [validUntil, value] = await hashmap.getByKey(3n);
+        expect(validUntil).toEqual(3000n);
+        expect(value).toEqualSlice(
+            beginCell().storeUint(345, 16).endCell().asSlice()
+        );
     
-    //     blockchain.now = 3001;
+        blockchain.now = 3001;
     
-    //     await hashmap.sendClearOldValues(deployer.getSender(), toNano('0.05'), {
-    //         queryId: 123n,
-    //     });
+        await hashmap.sendClearOldValues(deployer.getSender(), toNano('0.05'), {
+            queryId: 123n,
+        });
     
-    //     await expect(hashmap.getByKey(2n)).rejects.toThrow();
-    //     await expect(hashmap.getByKey(3n)).rejects.toThrow();
-    // });
+        await expect(hashmap.getByKey(2n)).rejects.toThrow();
+        await expect(hashmap.getByKey(3n)).rejects.toThrow();
+    });
 
-    // it('should throw on wrong opcode', async () => {
-    //     const result = await deployer.send({
-    //         to: hashmap.address,
-    //         value: toNano('0.05'),
-    //         body: beginCell().storeUint(123, 32).storeUint(123, 64).endCell(),
-    //     })
+    it('should throw on wrong opcode', async () => {
+        const result = await deployer.send({
+            to: hashmap.address,
+            value: toNano('0.05'),
+            body: beginCell().storeUint(123, 32).storeUint(123, 64).endCell(),
+        })
 
-    //     expect(result.transactions).toHaveTransaction({
-    //         from: deployer.address,
-    //         to: hashmap.address,
-    //         exitCode: 12,
-    //     })
-    // })
+        expect(result.transactions).toHaveTransaction({
+            from: deployer.address,
+            to: hashmap.address,
+            exitCode: 12,
+        })
+    })
 
     it('should throw on bad query', async () => {
         const result = await deployer.send({
@@ -155,7 +155,7 @@ describe('Hashmap', () => {
         expect(result.transactions).toHaveTransaction({
             from: deployer.address,
             to: hashmap.address,
-            exitCode: 12,
+            success: false,
         });
     })
     
